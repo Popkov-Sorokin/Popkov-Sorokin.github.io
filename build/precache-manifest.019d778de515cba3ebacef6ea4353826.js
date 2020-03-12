2dfe74b723388bfbbdc2c4cb8faae9912d068202
Popkov-Sorokin <popkov-fzf@yandex.ru>
2020-03-12 18:34:34 +0300

    homepage now is 'build'

---
+++ build/precache-manifest.019d778de515cba3ebacef6ea4353826.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f69c1c973ce129780682ed292da205",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "f6a02a9327fb0a283d16",
+    "url": "/build/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "025dbc12d55c95a83ff6",
+    "url": "/build/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f6a02a9327fb0a283d16",
+    "url": "/build/static/js/2.d7bdf24b.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/build/static/js/2.d7bdf24b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "025dbc12d55c95a83ff6",
+    "url": "/build/static/js/main.85fb9ebf.chunk.js"
+  },
+  {
+    "revision": "c85433513211f85899fa",
+    "url": "/build/static/js/runtime-main.1f70b76b.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/build/static/media/persik.4e1ec840.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.45894a785ffaeb0a7f0226a207972cb3.js"
+  "/build/precache-manifest.019d778de515cba3ebacef6ea4353826.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
